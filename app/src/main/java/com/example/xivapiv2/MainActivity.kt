3697388067caeef70025b9d57ec7be45b1cc264b
Louis JEANTY <louis.jeanty@etu.univ-lyon1.fr>
2020-09-25 14:02:23 +0200

    Modif Text Button

--- app/src/main/java/com/example/xivapiv2/MainActivity.kt
+++ app/src/main/java/com/example/xivapiv2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -47,6 +48,7 @@ class MainActivity : AppCompatActivity() {
         mGoogleSignInClient.signOut()
             .addOnCompleteListener(this) {
                 // Update your UI here
+
             }
     }
 
@@ -64,6 +66,9 @@ class MainActivity : AppCompatActivity() {
             val account = completedTask.getResult(
                 ApiException::class.java
             )
+
+            val monBouton = findViewById<Button>(R.id.google_login_btn)
+            monBouton.text = "Connecté"
             // Signed in successfully
             val googleId = account?.id ?: ""
             Log.i("Google ID",googleId)
